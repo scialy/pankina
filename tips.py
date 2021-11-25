11d193d8e6123ffc669b1e4ea6c76ed8e6980437
scialy <74175158+scialy@users.noreply.github.com>
2021-11-25 19:59:19 +0200

    Update tips.py

--- tips.py
+++ tips.py
@@ -99,7 +99,7 @@ else:
 barman_tip = (total_tip * (1-ahuz))/total_hours_barmanim
 
 #Parametro ahmash
-parametro_ahmash = 5
+parametro_ahmash = 0
 melzar_tip = (total_tip * ahuz)/(total_hours_melzarim+total_hours_ahmashim/parametro_ahmash)
 ahmash_tip = melzar_tip/parametro_ahmash
 
